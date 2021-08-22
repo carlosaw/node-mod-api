a2c099bd448ca657184c0a9665ca0c42c7ec6cba
unknown <carlosfreevg@gmail.com>
2021-08-22 10:36:10 -0400

    create phrase

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+import { Phrase } from '../models/Phrase';
+
+
+export const ping = (req: Request, res: Response) => {
+  res.json({pong: true});
+}
+
+export const random = (req: Request, res: Response) => {
+  let nRand: number = Math.floor( Math.random() * 10 );
+  res.json({number: nRand});
+}
+
+export const nome = (req: Request, res: Response) => {
+  let nome: string = req.params.nome;
+  res.json({nome: `Você enviou o nome ${nome}`});
+}
+
+export const createPhrase = async (req: Request, res: Response) => {
+  // Receber dados enviados
+  //console.log(req.body);
+  let { author, txt } = req.body;
+
+  let newPhrase = await Phrase.create({ author, txt });
+
+  res.json({ id: newPhrase.id, author, txt  });
+}
